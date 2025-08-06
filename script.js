c59f1c0702e7c2958a03728d8b1cac1a1048c807
Threed95 <bolshanovad95@gmail.com>
2025-08-06 23:18:57 +0300

    Update script.js
    функция отображения слов, функция генерации случайных слов, функция подсчёта ошибок

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const divWord = document.querySelector(".word");
 let currentIndex = 0;
 let currentWord = "";
 const words = ["apple", "javascript", "keyboard", "function", "hometask", "example", "student"];
+const correctDisplay = document.querySelector(".correct-count");
+const wrongDisplay = document.querySelector(".wrong-count");
+let wordMistakes = document.querySelector(".word-mistakes");
+let correctCount = 0;
+let wrongCount = 0;
+let mistakesScore = 0;
+
 
 function generateWord() {
     const random = Math.floor(Math.random() * words.length);
@@ -19,16 +26,41 @@ function displayWord() {
     }
 }
 
+function updateScore() {
+    correctDisplay.textContent = correctCount;
+
+}
+
+function makeMistakes() {
+    wrongDisplay.textContent = wrongCount;
+    wordMistakes.textContent = mistakesScore;
+}
+
+
 function enterWord(event) {
     const pressKey = event.key;
     if (pressKey === currentWord[currentIndex]) {
         const spans = divWord.getElementsByTagName("span");
         spans[currentIndex].classList.add("c");
         currentIndex++;
-        if (currentIndex === currentWord.length) {
-            displayWord();
-        }
+    } else {
+        const spans = divWord.getElementsByTagName("span");
+        spans[currentIndex].classList.add("w");
+        wrongCount++;
+        mistakesScore++;
+        makeMistakes();
     }
+    if (currentIndex === currentWord.length) {
+        displayWord();
+        correctCount++;
+        updateScore();
+    }
+    if (correctDisplay.textContent === "5") {
+        alert("Вы выиграли!");
+    } else if (wrongDisplay.textContent === "5") {
+        alert("Вы проиграли!");
+    }
+
 }
 
 
